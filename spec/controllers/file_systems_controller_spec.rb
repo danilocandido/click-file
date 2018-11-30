24116294cf63c51a7e70f22ecb8f0b4858cabd22
Danilo Cândido <danilod07@gmail.com>
2018-11-30 09:11:51 -0300

    post create

--- spec/controllers/file_systems_controller_spec.rb
+++ spec/controllers/file_systems_controller_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe FileSystemsController, type: :controller do
+  let(:files) { [] }
+
+  before do
+    3.times do |n|
+      directory = create(:directory)
+      files << directory
+      directory = nil if n.even?
+      files << create(:file, folder: directory)
+    end
+  end
 
   describe 'GET #index' do
-    let(:files) { [] }
 
-    before do
-      3.times do |n|
-        directory = create(:directory)
-        files << directory
-        directory = nil if n.even?
-        files << create(:file, folder: directory)
-      end
-    end
 
     it 'renders :index template' do
       get :index
@@ -23,6 +24,13 @@ RSpec.describe FileSystemsController, type: :controller do
       get :index
       files_with_no_parents = files.select {|n| n.folder.nil? }
       expect(assigns(:file_systems)).to match_array files_with_no_parents
+    end
+  end
+
+  describe 'POST #create' do
+    it 'renders root path' do
+      post :create, params: { file_system_id: files.last, name: 'pasta', it_is_folder: 'Salvar'  }
+      expect(response).to redirect_to(root_path)
     end
   end
 end
